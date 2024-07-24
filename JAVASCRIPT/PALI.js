cce85a261db9a7a4448bac7ccdc3f430b0b1e3ba
vaxanth01 <vaxanth01@gmail.com>
2024-07-24 17:52:57 +0530

    js completed

---
+++ JAVASCRIPT/PALI.js
@@ -0,0 +1,17 @@
+// var pali="sdam"
+
+// reverse_pali = pali.split('').reverse().join('');
+
+// // var result = pali === reverse_pali?pali+" yes palindrome":pali+" not palindrome";
+// if (pali === reverse_pali){
+//     console.log("yes palindrome")
+// }
+// else{
+//     console.log("no palindrome")
+
+// }
+// // console.log(result)
+
+
+c=eval(8-3)
+console.log(c)
